30f95279103a55280b55c276293d7e82773cfe94
aksen510 <510cara@naver.com>
2022-12-12 13:41:16 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Info_custom from "./Info_custom";
+import "./App.css";
+
+const App = () => {
+  return <Info_custom />;
+};
+
+export default App;
---
+++ src/Info_custom.js
@@ -0,0 +1,29 @@
+import useInputs from "./useInputs";
+
+const Info_custom = () => {
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+
+  const { name, nickname } = state;
+
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChange} />
+        <input value={nickname} onChange={onChange} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임:</b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info_custom;
